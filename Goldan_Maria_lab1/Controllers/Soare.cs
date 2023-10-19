f8b04408d72c3763ace113d5e467ecb178585d31
Maria s Computer <Maria s Computer@MARIA>
2023-10-19 18:17:55 +0300

    controller nou

---
+++ Goldan_Maria_lab1/Controllers/Soare.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Goldan_Maria_lab1.Controllers
+{
+    public class Soare : Controller
+    {
+        public string Index()
+        {
+            return "Buna dimineata soare!";
+        }
+
+        public string Salut()
+        {
+            return "Salut utilizatorule!";
+        }
+
+        public string Afisare(string n, int i)
+        {
+            return "S-a specificat cuvantul " + n + " si numarul " + i + ".";
+        }
+    }
+}
